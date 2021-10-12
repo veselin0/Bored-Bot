e14b9a2c9328f76f970c9d9271bbdc4167209280
Veselin <vesselin60@gmail.com>
2021-10-12 12:04:01 +0300

    event listener addet to tne button

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 /**
 Challenge: 
 
-- Start building out the BoredBot Skeleton however you'd like. 
-    That will include:
-    - A title for the app ("BoredBot" might be a good start 😉)
-    - A placeholder element that will be populated with the random 
-      idea we get from the API
-    - A button to click for triggering the GET request to the Bored API. 
-      (Don't worry about implementing the button quite yet)
+When the button is clicked, call out to the Bored API
+(URL: https://apis.scrimba.com/bored/api/activity)
+and replace the h4 with the activity from the API
+
 */
 
-//"https://apis.scrimba.com/bored/api/activity" at scrimba's server
+const btn = document.querySelector(".btn");
+btn.addEventListener('click', () => 
 fetch("https://www.boredapi.com/api/activity") // at boredapi server
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        document.getElementById("placeholder-element").textContent = data.activity;
-    });
+        document.querySelector(".placeholder-element").textContent = data.activity;
+    }));
